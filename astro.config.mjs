840398de702bf85719d2adaf4f424cee5ff0e06b
Vic-Zmw <vic_zhang2080@foxmail.com>
2026-01-16 15:00:51 +0800

    更新个人信息与配置，准备部署

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://Vic-Zmw.github.io',
+  
+  base: '/my-portfolio',
+  
+  trailingSlash: 'always',
+  
+  vite: {
+      plugins: [tailwindcss()],
+  },
+  
+  integrations: [react()]
+});
